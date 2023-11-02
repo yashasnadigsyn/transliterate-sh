39b34e7b6d5029de24371349ec571bd7de75ab50
Yashas Nadig <103478177+yashasnadigsyn@users.noreply.github.com>
2023-11-02 15:59:47 +0530

    Update transli.sh

--- transli.sh
+++ transli.sh
@@ -3,11 +3,11 @@ lang="hi"
 
 xdotool keyup alt+q
 windowid=$(xdotool getwindowfocus)
-xdotool sleep 0.1 windowactivate --sync $windowid key ctrl+shift+Left
-sleep 0.1 && texty=$(xsel -p | xargs)
+xdotool sleep 0.2 windowactivate --sync $windowid key ctrl+shift+Left
+sleep 0.2 && texty=$(xsel -p | xargs)
 selections=$(curl -s "https://inputtools.google.com/request?text=$texty&itc=$lang-t-i0&num=6" | jq -r '.[1][0][1][] ')
 printf "%s\n" "$selections"
 selected_item=$(echo "$selections" | tr ' ' '\n' | rofi -dmenu -p "Select: ")
-sleep 0.1 && windowid=$(xdotool getwindowfocus)
-xdotool sleep 0.1 windowactivate --sync $windowid type "$selected_item"
+sleep 0.2 && windowid=$(xdotool getwindowfocus)
+xdotool sleep 0.2 windowactivate --sync $windowid type "$selected_item"
 
